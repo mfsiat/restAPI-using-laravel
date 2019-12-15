e1037ff81941eaa2ef90c1f0fe1acb486a53b9b4
MD. Nasirul Islam <nasirul.islam@northsouth.edu>
2019-12-15 12:40:16 +0600

    done with the full crud

--- back-end/app/Http/Controllers/ItemsController.php
+++ back-end/app/Http/Controllers/ItemsController.php
@@ -101,6 +101,10 @@ class ItemsController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $item = Item::find($id);
+        $item->delete();
+
+        $response = array('response' => 'Item Deleted', 'success' => true);
+        return $response;
     }
 }
