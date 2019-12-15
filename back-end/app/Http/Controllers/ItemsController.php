01c50595a8bd63372fb6fd910405492072e90b9e
MD. Nasirul Islam <nasirul.islam@northsouth.edu>
2019-12-15 12:17:51 +0600

    added update for put method

--- back-end/app/Http/Controllers/ItemsController.php
+++ back-end/app/Http/Controllers/ItemsController.php
@@ -22,16 +22,6 @@ class ItemsController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -40,7 +30,7 @@ class ItemsController extends Controller
     public function store(Request $request)
     {
         // to post any kind if item
-        // follwing code represent normal post method for our api
+        // following code represent normal post method for our api
         // but this can not handle error handling
         // $this->validate($request), [
         //   'text' => 'required'
@@ -77,17 +67,6 @@ class ItemsController extends Controller
     }
 
     /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -96,7 +75,22 @@ class ItemsController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'text' => 'required'
+          ]);
+  
+          if($validator->fails()){
+            $response = array('response' => $validator->messages(), 'success' => false);
+            return $response;
+          } else {
+            // Find item 
+            $item = Item::find($id);
+            $item->text = $request->input('text');
+            $item->body = $request->input('body');
+            $item->save();
+  
+            return response()->json($item);
+          }        
     }
 
     /**
